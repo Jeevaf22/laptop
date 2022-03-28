5295f039fba47d38a83d385d3a593ffb786df8cf
Jeeevaf22 <jeevanandam@f22labs.com>
2022-03-28 16:37:12 +0530

    Link

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,8 +1,10 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
   return (
     <div className="links">
-      <a href="/">Home</a>
-      <a href="/about">About</a>
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
     </div>
   );
 };
