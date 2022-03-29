18438394ab57a2ce0a08f23a83262bbbe8724523
Jeeevaf22 <jeevanandam@f22labs.com>
2022-03-29 11:40:37 +0530

    heading changed

--- src/About.js
+++ src/About.js
@@ -1,7 +1,7 @@
 const About = () => {
   return (
     <div className="about">
-      <h2>About</h2>
+      <h1>About</h1>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse sed ipsam
         architecto facilis enim consectetur magni accusamus vel, quod quidem
