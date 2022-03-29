8dba300b9a7c1895071d6c33a760440fbf95f1ee
Jeevandandam M <jeevandandam@F22Labss-Mac-mini.local>
2022-03-29 11:18:17 +0530

    about contents added

--- src/About.js
+++ src/About.js
@@ -8,6 +8,18 @@ const About = () => {
         accusantium possimus rem eligendi consequatur. Ipsum necessitatibus
         doloribus praesentium repudiandae!
       </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse sed ipsam
+        architecto facilis enim consectetur magni accusamus vel, quod quidem
+        accusantium possimus rem eligendi consequatur. Ipsum necessitatibus
+        doloribus praesentium repudiandae!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse sed ipsam
+        architecto facilis enim consectetur magni accusamus vel, quod quidem
+        accusantium possimus rem eligendi consequatur. Ipsum necessitatibus
+        doloribus praesentium repudiandae!
+      </p>
     </div>
   );
 };
