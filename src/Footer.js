c4aebfeb9e281c469b3b512a9d35f6145b9abddf
Jeeevaf22 <jeevanandam@f22labs.com>
2022-03-30 12:07:20 +0530

    footer added

--- src/Footer.js
+++ src/Footer.js
@@ -6,19 +6,19 @@ const Footer = () => {
         <a href="#">
           <img
             src="https://cdn3.iconfinder.com/data/icons/2018-social-media-logotypes/1000/2018_social_media_popular_app_logo_twitter-256.png"
-            alt=""
+            alt="twitter"
           />
         </a>
         <a href="#">
           <img
             src="https://cdn2.iconfinder.com/data/icons/social-media-2285/512/1_Whatsapp2_colored_svg-256.png"
-            alt=""
+            alt="whatsapp"
           />
         </a>
         <a href="#">
           <img
             src="https://cdn3.iconfinder.com/data/icons/capsocial-round/500/facebook-256.png"
-            alt=""
+            alt="facebook"
           />
         </a>
       </div>
