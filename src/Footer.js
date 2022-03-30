67ecc3c8971bdaf5e65f2eedc6b2fe245a07e220
Jeeevaf22 <jeevanandam@f22labs.com>
2022-03-30 12:09:27 +0530

    footer added

--- src/Footer.js
+++ src/Footer.js
@@ -1,26 +1,28 @@
+import { Link } from "react-router-dom";
+
 const Footer = () => {
   return (
     <div className="footer">
       <h3>Lorem ipsum dolor sit amet. Social Networks</h3>
       <div>
-        <a href="#">
+        <Link to="#">
           <img
             src="https://cdn3.iconfinder.com/data/icons/2018-social-media-logotypes/1000/2018_social_media_popular_app_logo_twitter-256.png"
             alt="twitter"
           />
-        </a>
-        <a href="#">
+        </Link>
+        <Link to="#">
           <img
             src="https://cdn2.iconfinder.com/data/icons/social-media-2285/512/1_Whatsapp2_colored_svg-256.png"
             alt="whatsapp"
           />
-        </a>
-        <a href="#">
+        </Link>
+        <Link to="#">
           <img
             src="https://cdn3.iconfinder.com/data/icons/capsocial-round/500/facebook-256.png"
             alt="facebook"
           />
-        </a>
+        </Link>
       </div>
     </div>
   );
