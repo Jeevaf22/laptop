218e7ae1c5505e1c1f5b820c556a9c6f97602d52
Jeevandandam M <jeevandandam@F22Labss-Mac-mini.local>
2022-04-05 10:28:44 +0530

    console removed

--- src/ProductDetail.js
+++ src/ProductDetail.js
@@ -5,8 +5,6 @@ const ProductDetail = () => {
   const { id } = useParams();
   const { data: product } = useFetch("http://localhost:8000/products/" + id);
 
-  console.log(product);
-
   return (
     <div className="productdetail">
       {product && (
