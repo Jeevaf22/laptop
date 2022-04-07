16618a118deadce5813f33803bd2c775df4833f4
jeevanandam1 <jeevanandam@f22labs.com>
2022-04-07 14:50:34 +0530

    last commit

--- src/About.js
+++ src/About.js
@@ -30,7 +30,7 @@ const About = () => {
         quod quidem accusantium possimus rem eligendi consequatur. Ipsum
         necessitatibus doloribus praesentium repudiandae!
       </p>
-      <h2>Lorem ipsum dolor sit.</h2>
+      <h2>Lorem ipsum door sit.</h2>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse sed ipsam
         architecto facilis enim <span> consectetur magni</span> accusamus vel,
